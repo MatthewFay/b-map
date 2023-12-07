c54ca387ec2a35de71f0b588c468a88e57c43380
MatthewFay <mefay@hotmail.com>
2023-12-06 19:37:28 -0800

    add tests

--- test/BMap.test.ts
+++ test/BMap.test.ts
@@ -12,4 +12,41 @@ describe('test BMap', () => {
       expect(actual).toStrictEqual('[[1,2],["test","test2"]]')
     })
   })
+
+  describe('test event system', () => {
+    test('add event', () => {
+      const bmap = new BMap()
+      const listener = jest.fn()
+      bmap.on('add', listener)
+
+      bmap.set(1, 2)
+
+      expect(listener).toHaveBeenCalledTimes(1)
+      expect(listener).toHaveBeenCalledWith(1, 2)
+    })
+
+    test('update event', () => {
+      const bmap = new BMap()
+      const listener = jest.fn()
+      bmap.on('update', listener)
+
+      bmap.set(1, 2)
+      bmap.set(1, 3)
+
+      expect(listener).toHaveBeenCalledTimes(1)
+      expect(listener).toHaveBeenCalledWith(1, 3)
+    })
+
+    test('delete event', () => {
+      const bmap = new BMap()
+      const listener = jest.fn()
+      bmap.on('delete', listener)
+
+      bmap.set(1, 2)
+      bmap.delete(1)
+
+      expect(listener).toHaveBeenCalledTimes(1)
+      expect(listener).toHaveBeenCalledWith(1, 2)
+    })
+  })
 })
