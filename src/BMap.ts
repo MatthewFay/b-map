b6a699084299033fa4ff4842d06f03277a7f88f4
MatthewFay <mefay@hotmail.com>
2023-12-10 16:58:47 -0800

    improve events, add batch ops

--- src/BMap.ts
+++ src/BMap.ts
@@ -1,13 +1,18 @@
 /**
  * Event types
  */
-type EventType = 'add' | 'update' | 'delete'
+export type BMapEventType = 'add' | 'update' | 'delete'
+
+/**
+ * Event listener. `entries` contains the added/updated/deleted entries.
+ */
+export type BMapListener<K, V> = (entries: BMap<K, V>) => void
 
 /**
  * Better Map()
  */
 export class BMap<K, V> extends Map<K, V> {
-  private readonly listeners: Map<EventType, Function[]> = new Map()
+  private readonly _listeners: Map<BMapEventType, Array<BMapListener<K, V>>> = new Map()
 
   /**
    * Custom serialization function
@@ -21,41 +26,102 @@ export class BMap<K, V> extends Map<K, V> {
   /**
    * Register listener
    */
-  on (event: EventType, callback: (key: K, value: V) => void): void {
-    if (!this.listeners.has(event)) {
-      this.listeners.set(event, [])
+  on (event: BMapEventType, listener: BMapListener<K, V>): this {
+    if (!this._listeners.has(event)) {
+      this._listeners.set(event, [])
     }
-    this.listeners.get(event)?.push(callback)
+    this._listeners.get(event)?.push(listener)
+    return this
   }
 
   /**
    * Notify subscribers
    */
-  private notify (event: EventType, key: K, value: V): void {
-    const eventListeners = this.listeners.get(event)
+  private _notify (event: BMapEventType, entries: BMap<K, V>): void {
+    const eventListeners = this._listeners.get(event)
     if (eventListeners != null) {
-      eventListeners.forEach((listener) => listener(key, value))
+      eventListeners.forEach((listener) => listener(entries))
     }
   }
 
-  set (key: K, value: V): this {
+  /**
+   * Set key-value, return whether it's new.
+   */
+  private _set (key: K, value: V): boolean {
     const isNewKey = !this.has(key)
     super.set(key, value)
-    if (isNewKey) {
-      this.notify('add', key, value)
-    } else {
-      this.notify('update', key, value)
+    return isNewKey
+  }
+
+  set (key: K, value: V): this {
+    const isNewKey = this._set(key, value)
+    if (this._listeners?.size > 0) {
+      this._notify(isNewKey ? 'add' : 'update', new BMap([[key, value]]))
     }
     return this
   }
 
-  delete (key: K): boolean {
+  /**
+   * Batch add/update entries
+   */
+  bSet (entries: Array<[K, V]>): this {
+    const newEntries: Array<[K, V]> = []
+    const updatedEntries: Array<[K, V]> = []
+    entries.forEach(([key, value]) => this._set(key, value) ? newEntries.push([key, value]) : updatedEntries.push([key, value]))
+    if (newEntries.length > 0) {
+      this._notify('add', new BMap(newEntries))
+    }
+    if (updatedEntries.length > 0) {
+      this._notify('update', new BMap(updatedEntries))
+    }
+    return this
+  }
+
+  /**
+   * Delete by key. Returns deleted entry if key existed
+   */
+  private _delete (key: K): [K, V] | undefined {
     if (this.has(key)) {
-      const value = this.get(key)
+      const value = this.get(key) as V
       super.delete(key)
-      this.notify('delete', key, value as V)
+      return [key, value]
+    }
+    return undefined
+  }
+
+  delete (key: K): boolean {
+    const entry = this._delete(key)
+    if (entry != null) {
+      this._notify('delete', new BMap([[key, entry[1]]]))
       return true
     }
     return false
+  }
+
+  /**
+   * Batch delete entries by key
+   */
+  bDelete (keys: K[]): boolean[] {
+    const deletedEntries: Array<[K, V]> = []
+    const result = keys.map(key => {
+      const entry = this._delete(key)
+      if (entry != null) {
+        deletedEntries.push(entry)
+        return true
+      }
+      return false
+    })
+    if (deletedEntries.length > 0) {
+      this._notify('delete', new BMap(deletedEntries))
+    }
+    return result
+  }
+
+  clear (): void {
+    const entries = new BMap(this)
+    super.clear()
+    if (entries.size > 0) {
+      this._notify('delete', entries)
+    }
   }
 }
